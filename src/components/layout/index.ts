fdb7d9ccf1c8ff8012bdbc9d0324ae386e0982be
Paullo Mourá <110057903+PauloRMFeitosa@users.noreply.github.com>
2026-06-09 10:52:06 -0300

    Reenviar projeto do CD

--- src/components/layout/index.ts
+++
@@ -1,2 +0,0 @@
-// Componentes de layout e roteamento
-export { ProtectedRoute } from "@/components/ProtectedRoute";
--- src/features/leituras/components/index.ts
+++
@@ -1,3 +0,0 @@
-// Componentes do domínio de leituras
-export { ProgressoBar } from "@/components/leituras/ProgressoBar";
-export { TagsInput } from "@/components/leituras/TagsInput";
